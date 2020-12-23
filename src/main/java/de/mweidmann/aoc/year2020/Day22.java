4cc1c8fbf05e6e7012ead5f0b60ca8bcdffb1d20
Michael Weidmann <michaelweidmann@web.de>
2020-12-24 00:10:09 +0100

    beautifying solution day 22 and added task description

--- src/main/java/de/mweidmann/aoc/year2020/Day22.java
+++ src/main/java/de/mweidmann/aoc/year2020/Day22.java
@@ -1,6 +1,5 @@
 package de.mweidmann.aoc.year2020;
 
-import javax.annotation.Nullable;
 import java.util.*;
 import java.util.stream.IntStream;
 
@@ -11,16 +10,16 @@ import java.util.stream.IntStream;
  */
 public class Day22 extends AbstractDay2020 {
 
-    private final LinkedList<Integer> initialCardsPlayer1;
-    private final LinkedList<Integer> initialCardsPlayer2;
+    private final LinkedList<Integer> stackPlayer1;
+    private final LinkedList<Integer> stackPlayer2;
 
     /**
      * Default constructor for Day21.
      */
     public Day22() {
         super(22);
-        this.initialCardsPlayer1 = this.getInitialCards(1);
-        this.initialCardsPlayer2 = this.getInitialCards(2);
+        this.stackPlayer1 = this.getInitialStacks(1);
+        this.stackPlayer2 = this.getInitialStacks(2);
     }
 
     /**
@@ -29,7 +28,7 @@ public class Day22 extends AbstractDay2020 {
      * @param playerNumber The player number from which the deck should be parsed.
      * @return A list containing the parsed cards of a player.
      */
-    private LinkedList<Integer> getInitialCards(int playerNumber) {
+    private LinkedList<Integer> getInitialStacks(int playerNumber) {
         LinkedList<Integer> cards = new LinkedList<>();
 
         String[] cardsSplittedByPlayer = this.INPUT_AS_STRING.split("\n\n");
@@ -44,40 +43,66 @@ public class Day22 extends AbstractDay2020 {
 
     @Override
     protected Object partOne() {
-        LinkedList<Integer> stackPlayer1 = new LinkedList<>(initialCardsPlayer1);
-        LinkedList<Integer> stackPlayer2 = new LinkedList<>(initialCardsPlayer2);
+        LinkedList<Integer> stackPlayer1 = new LinkedList<>(this.stackPlayer1);
+        LinkedList<Integer> stackPlayer2 = new LinkedList<>(this.stackPlayer2);
 
         while(!stackPlayer1.isEmpty() && !stackPlayer2.isEmpty()) {
-            processRound(stackPlayer1, stackPlayer2, null);
+            int cardPlayer1 = stackPlayer1.removeFirst();
+            int cardPlayer2 = stackPlayer2.removeFirst();
+
+            processRound(stackPlayer1, stackPlayer2, cardPlayer1, cardPlayer2);
         }
 
-        return calculateEndResult(stackPlayer1.isEmpty() ? stackPlayer2 : stackPlayer1);
+        return calculateScore(stackPlayer1.isEmpty() ? stackPlayer2 : stackPlayer1);
     }
 
-    private void processRound(LinkedList<Integer> stackPlayer1, LinkedList<Integer> stackPlayer2, @Nullable RoundWinner roundWinner) {
-        int cardPlayer1 = stackPlayer1.removeFirst();
-        int cardPlayer2 = stackPlayer2.removeFirst();
-
-        if (roundWinner != null) {
-            if (roundWinner.equals(RoundWinner.PLAYER_1)) {
-                stackPlayer1.addLast(cardPlayer1);
-                stackPlayer1.addLast(cardPlayer2);
-            } else if (roundWinner.equals(RoundWinner.PLAYER_2)) {
-                stackPlayer2.addLast(cardPlayer2);
-                stackPlayer2.addLast(cardPlayer1);
-            }
+    /**
+     * Processes a round by checking which of the top card of both players is higher.
+     * Based on this decision the cards are added to the winning stack of this round.
+     *
+     * @param stackPlayer1 The card stack of player 1.
+     * @param stackPlayer2 The card stack of player 2.
+     * @param cardPlayer1 The top card of player 1.
+     * @param cardPlayer2 The top card of player 2.
+     */
+    private void processRound(LinkedList<Integer> stackPlayer1, LinkedList<Integer> stackPlayer2, int cardPlayer1, int cardPlayer2) {
+        if (cardPlayer1 > cardPlayer2) {
+            stackPlayer1.addLast(cardPlayer1);
+            stackPlayer1.addLast(cardPlayer2);
         } else {
-            if (cardPlayer1 > cardPlayer2) {
-                stackPlayer1.addLast(cardPlayer1);
-                stackPlayer1.addLast(cardPlayer2);
-            } else {
-                stackPlayer2.addLast(cardPlayer2);
-                stackPlayer2.addLast(cardPlayer1);
-            }
+            stackPlayer2.addLast(cardPlayer2);
+            stackPlayer2.addLast(cardPlayer1);
         }
     }
 
-    private long calculateEndResult(LinkedList<Integer> winnerStack) {
+    /**
+     * Processes a round by checking the winner of the round passed by a parameter.
+     * Based on the winner the cards are added to the winning stack of this round.
+     *
+     * @param stackPlayer1 The card stack of player 1.
+     * @param stackPlayer2 The card stack of player 2.
+     * @param cardPlayer1 The top card of player 1.
+     * @param cardPlayer2 The top card of player 2.
+     * @param winner The winner of this round.
+     */
+    private void processRound(LinkedList<Integer> stackPlayer1, LinkedList<Integer> stackPlayer2, int cardPlayer1, int cardPlayer2, Winner winner) {
+        if (winner == Winner.PLAYER_1) {
+            stackPlayer1.addLast(cardPlayer1);
+            stackPlayer1.addLast(cardPlayer2);
+        } else {
+            stackPlayer2.addLast(cardPlayer2);
+            stackPlayer2.addLast(cardPlayer1);
+        }
+    }
+
+    /**
+     * Calculates the end score of the game.
+     * The bottom card in their deck is worth the value of the card multiplied by 1, the second-from-the-bottom card is worth the value of the card multiplied by 2, and so on.
+     *
+     * @param winnerStack The stack of the winner.
+     * @return The calculated score.
+     */
+    private int calculateScore(LinkedList<Integer> winnerStack) {
         return IntStream.range(0, winnerStack.size())
                 .map(index -> winnerStack.get(index) * (winnerStack.size() - index))
                 .sum();
@@ -85,34 +110,42 @@ public class Day22 extends AbstractDay2020 {
 
     @Override
     protected Object partTwo() {
-        rec(initialCardsPlayer1, initialCardsPlayer2);
-        return calculateEndResult(initialCardsPlayer1.isEmpty() ? initialCardsPlayer2 : initialCardsPlayer1);
+        recurseCombat(stackPlayer1, stackPlayer2);
+        return calculateScore(stackPlayer1.isEmpty() ? stackPlayer2 : stackPlayer1);
     }
 
-    private RoundWinner rec(LinkedList<Integer> stackPlayer1, LinkedList<Integer> stackPlayer2) {
+    /**
+     * This method plays a round of recursive combat. Rules can be found in the description of this task.
+     * Important points will be documented in the code lines.
+     *
+     * @param stackPlayer1 The card stack of player 1.
+     * @param stackPlayer2 The card stack of player 2.
+     * @return The winner of the game.
+     */
+    private Winner recurseCombat(LinkedList<Integer> stackPlayer1, LinkedList<Integer> stackPlayer2) {
         Set<List<Integer>> previousConstellations = new HashSet<>();
 
         while(!stackPlayer1.isEmpty() && !stackPlayer2.isEmpty()) {
             if (!previousConstellations.add(new ArrayList<>(stackPlayer1))) {
-                return RoundWinner.PLAYER_1;
+                return Winner.PLAYER_1;
             }
 
-            int cardPlayer1 = stackPlayer1.getFirst();
-            int cardPlayer2 = stackPlayer2.getFirst();
+            int cardPlayer1 = stackPlayer1.removeFirst();
+            int cardPlayer2 = stackPlayer2.removeFirst();
 
-            RoundWinner roundWinner = null;
-
-            if (cardPlayer1 <= stackPlayer1.size() - 1 && cardPlayer2 <= stackPlayer2.size() - 1) {
-                roundWinner = rec(new LinkedList<>(stackPlayer1.subList(1, cardPlayer1 + 1)), new LinkedList<>(stackPlayer2.subList(1, cardPlayer2 + 1)));
+            // If both players have at least as many cards remaining in their deck as the value of the card, then a another "Sub-Game" is started.
+            if (cardPlayer1 <= stackPlayer1.size() && cardPlayer2 <= stackPlayer2.size()) {
+                Winner winner = recurseCombat(new LinkedList<>(stackPlayer1.subList(0, cardPlayer1)), new LinkedList<>(stackPlayer2.subList(0, cardPlayer2)));
+                processRound(stackPlayer1, stackPlayer2, cardPlayer1, cardPlayer2, winner);
+            } else {
+                processRound(stackPlayer1, stackPlayer2, cardPlayer1, cardPlayer2);
             }
-
-            processRound(stackPlayer1, stackPlayer2, roundWinner);
         }
 
-        return stackPlayer1.isEmpty() ? RoundWinner.PLAYER_2 : RoundWinner.PLAYER_1;
+        return stackPlayer1.isEmpty() ? Winner.PLAYER_2 : Winner.PLAYER_1;
     }
 
-    private enum RoundWinner {
+    private enum Winner {
         PLAYER_1,
         PLAYER_2
     }
