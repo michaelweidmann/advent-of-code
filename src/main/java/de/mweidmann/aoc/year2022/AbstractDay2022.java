37287558fb70c7e71ace270cfc200ed1252be827
Michael Weidmann <michaelweidmann@web.de>
2022-12-02 01:12:19 +0100

    add solution for day 1 2022

---
+++ src/main/java/de/mweidmann/aoc/year2022/AbstractDay2022.java
@@ -0,0 +1,15 @@
+package de.mweidmann.aoc.year2022;
+
+import de.mweidmann.aoc.utils.AbstractDay;
+
+/**
+ * The abstract representation of a day in the year 2022.
+ *
+ * @author mweidmann
+ */
+public abstract class AbstractDay2022 extends AbstractDay {
+
+    protected AbstractDay2022(int day) {
+        super(2022, day);
+    }
+}
---
+++ src/main/java/de/mweidmann/aoc/year2022/Day01.java
@@ -0,0 +1,63 @@
+package de.mweidmann.aoc.year2022;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.Function;
+
+/**
+ * Contains the solutions to the tasks from 01.12.2022.
+ *
+ * @author mweidmann
+ */
+public class Day01 extends AbstractDay2022 {
+
+    /**
+     * Default constructor for Day01.
+     */
+    public Day01() {
+        super(1);
+    }
+
+    @Override
+    protected Object partOne() {
+        var max = Long.MIN_VALUE;
+        var sum = 0L;
+
+        for (var inputLine : this.INPUT) {
+            if (!inputLine.isBlank()) {
+                sum += Long.parseLong(inputLine);
+                continue;
+            }
+
+            if (sum > max) {
+                max = sum;
+            }
+            sum = 0L;
+        }
+
+        return max;
+    }
+
+    @Override
+    protected Object partTwo() {
+        var maximumList = Arrays.asList(Long.MIN_VALUE, Long.MIN_VALUE, Long.MIN_VALUE);
+        var sum = 0L;
+
+        for (var inputLine : this.INPUT) {
+            if (!inputLine.isBlank()) {
+                sum += Long.parseLong(inputLine);
+                continue;
+            }
+
+            var smallestMaximumElement = Collections.min(maximumList);
+            var indexOfSmallestMaximumElement = maximumList.indexOf(smallestMaximumElement);
+            if (sum > smallestMaximumElement) {
+                maximumList.set(indexOfSmallestMaximumElement, sum);
+            }
+            sum = 0L;
+        }
+
+        return maximumList.stream().mapToLong(Long::longValue).sum();
+    }
+}
